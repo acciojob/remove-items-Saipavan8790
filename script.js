a23cbdfd834cd9310199a43c7c3ba24ce7756d74
Saipavan8790 <127079433+Saipavan8790@users.noreply.github.com>
2023-11-02 13:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-let select = document.getElementById("colorSelect");
-let btn =  document.querySelector("input[type=button]")
-btn.addEventListener("click", ()=>{       
-	//select.children--> will give all the child elements of select element
-    for (const child of select.children) {
-		//select.value--> will give value of the selected element
-        if(child.value === select.value){
-			//child.remove()-->remove the child element
-                child.remove();
-         }
+// let select = document.getElementById("colorSelect");
+// let btn =  document.querySelector("input[type=button]")
+// btn.addEventListener("click", ()=>{       
+// 	//select.children--> will give all the child elements of select element
+//     for (const child of select.children) {
+// 		//select.value--> will give value of the selected element
+//         if(child.value === select.value){
+// 			//child.remove()-->remove the child element
+//                 child.remove();
+//          }
+//     }
+// })
+
+
+      // 2nd Method
+
+//your JS code here. If required.
+let color=document.getElementById("colorSelect");
+document.querySelector.contains = function(selector, text) {
+  var elements = document.querySelectorAll(selector);
+  for (var i = 0; i < elements.length; i++) {
+    if (elements[i].textContent === text) {
+      return elements[i];
     }
-})
+  }
+  return null;
+};
+function remove(){
+       var elementToFind = document.querySelector.contains('option',color.value);
+    elementToFind.remove();
+
+}
